529f6c4ade2b6ba7420bd6b65200c179dc44005c
be-marc <marcbecker@posteo.de>
2026-07-02 17:05:22 +0200

    remove rounding from results processing

--- mbo/results.R
+++ mbo/results.R
@@ -60,54 +60,6 @@ aggregated = imap_dtr(results, function(results, task_id) {
   })
 }, .idcol = "task_id")
 
-cols = c("runtime_learners", "runtime_surrogate", "runtime_optimizer", "walltime", "cpu_hours")
-aggregated[, (cols) := map(.SD, round, 0), .SDcols = cols]
-cols = c("utilization", "performance")
-aggregated[, (cols) := map(.SD, signif, 2), .SDcols = cols]
-cols = c("mean_runtime_learners")
-aggregated[, (cols) := map(.SD, signif, 1), .SDcols = cols]
-
 setcolorder(aggregated, c("task_id", "algorithm", "runtime_learners", "runtime_surrogate", "runtime_optimizer", "mean_runtime_learners", "walltime", "cpu_hours", "evals", "performance", "utilization"))
 
 fwrite(aggregated, "mbo/results/aggregated.csv")
-
-# tab[, task_id := ifelse(duplicated(task_id), "", task_id)]
-
-# tab[, algorithm := fcase(
-#   algorithm == "cl_mbo",      "CLBO",
-#   algorithm == "central_mbo", "ACBO",
-#   algorithm == "async_mbo",   "ADBO",
-#   default = algorithm
-# )]
-
-# tab[, utilization := utilization * 100]
-
-# #knitr::kable(tab, format = "latex", booktabs = TRUE)
-
-# knitr::kable(tab[, list(task_id, algorithm, mean_runtime_learners, evals, utilization)], 
-#   format = "markdown", 
-#   col.names = c("Task", "Algorithm", "Mean Runtime Learners [s]", "Evaluations", "Utilization [%]"), format.args = list(big.mark = ","))
-
-# knitr::kable(tab[, list(task_id, algorithm, runtime_learners, runtime_surrogate, runtime_optimizer, mean_runtime_learners, walltime, cpu_hours, performance)], 
-#   format = "markdown", 
-#   col.names = c("Task", "Algorithm", "Runtime Learners", "Runtime Surrogate", "Runtime Optimizer", "Mean Runtime Learners [s]", "Walltime [s]", "CPU Hours", "Performance"), format.args = list(big.mark = ","))
-
-# plot_data = imap_dtr(results, function(results, task_id) {
-#   archive = rbindlist(results, fill = TRUE, idcol = "algorithm")
-#   archive_best = archive[!is.na(classif.ce)]
-#   archive_best[algorithm == "cl_mbo", walltime := as.numeric(difftime(timestamp, timestamp[1], units = "secs"))]
-#   archive_best[algorithm == "central_mbo", walltime := as.numeric(difftime(timestamp_ys, timestamp_ys[1], units = "secs"))]
-#   archive_best[algorithm == "async_mbo", walltime := as.numeric(difftime(timestamp_ys, timestamp_xs[1], units = "secs"))]
-#   archive_best[, incumbent := cummin(classif.ce), by = algorithm]
-#   archive_best
-# }, .idcol = "task_id", .fill = TRUE)
-
-
-# ggplot(plot_data, aes(x = walltime, y = incumbent, color = algorithm)) +
-#   geom_line() +
-#   scale_color_discrete(name = "Algorithm") +
-#   labs(x = "Walltime", y = "Performance") +
-#   facet_wrap(~task_id, scales = "free_y") +
-#   theme_minimal(base_size = 14)
-
-# ggsave("performance.png", width = 8, height = 6)
